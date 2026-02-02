576e7eceab9e3a8cb5694c0109e52e9c8fed3656
Ashish Srungaram <ashishsrungaram26@gmail.com>
2026-02-01 19:18:32 -0500

    chore: src updates

--- src/Fourth_Chapter/loops.js
+++ src/Fourth_Chapter/loops.js
@@ -3,3 +3,19 @@ while (i < 10) {
     i++;
     console.log(i);
 }
+
+let someArray = ["Ashish","Srungaram","Rammohan","Padmalatha","Abhinav"];
+let notFound = true;
+
+while(notFound && someArray.length > 0)
+{
+    if (someArray[0] === "Rammohan")
+    {
+        console.log("Found Him/Her!!");
+        notFound = false;
+    }
+    else
+    {
+        someArray.shift();
+    }
+}
